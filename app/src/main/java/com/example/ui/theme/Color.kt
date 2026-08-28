e066efd15f44b97b2cef4a9b60c7b14f58091a6f
yashkoparde <yashkoparde2022@gmail.com>
2026-08-28 09:15:00 +0530

    ui: define Bhagwa saffron primary color palette in Color.kt

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val StravaOrange = Color(0xFFFC4C02)
+val StravaOrangeLight = Color(0xFFFF6F22)
+val StravaOrangeDark = Color(0xFFC73600)
+
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkSurfaceVariant = Color(0xFF282828)
+
+val GoldStar = Color(0xFFFFD700)
+val SilverBadge = Color(0xFFC0C0C0)
+val BronzeBadge = Color(0xFFCD7F32)
+
+val TextPrimary = Color(0xFFF5F5F5)
+val TextSecondary = Color(0xFFA0A0A0)
+val MapGridColor = Color(0xFF333333)
+val MapRouteLine = Color(0xFFFC4C02)
+
